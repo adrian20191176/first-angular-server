e62cee16cf4f1b3aae6b9a065938443029643a46
unknown <adrian.20191176@iit.ac.lk>
2021-04-18 19:08:18 +0530

    Final

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
-'use strict';
-
-const Express = require('express');
+const express = require('express');
 const router = require('./lib');
 
-const app = Express();
-const port = process.env.PORT || 3000;
+const app = express(),
+      bodyParser = require("body-parser");
+      port = 3080;
+
+app.use(bodyParser.json());
+app.use(express.static(process.cwd()+"/practiceFrontEnd/dist/sample-project/"));
+
+
+app.get('/', (req,res) => {
+  res.sendFile(process.cwd()+"/practiceFrontEnd/dist/sample-project/index.html")
+});
 
 
 app.use('/api/', router);
